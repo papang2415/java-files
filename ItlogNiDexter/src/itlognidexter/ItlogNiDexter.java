c36e16146235d71e8f2d5ce95e67ae6fab5bb7cd
iampatik <janpatrick.tancinco@student.passerellesnumeriques.org>
2021-02-09 14:43:13 +0800

    update

--- ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
+++ ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
@@ -2,6 +2,20 @@ package itlognidexter;
 
 import java.util.Scanner;
 
+class Inventory {
+
+    static boolean customer;
+    String name;
+    int penoy, balut16, balut18;
+
+    public Inventory(String name, int penoy, int balut16, int balut18) {
+        this.name = name;
+        this.penoy = penoy;
+        this.balut16 = balut16;
+        this.balut18 = balut18;
+    }
+}
+
 public class ItlogNiDexter {
 
     public static void main(String[] args) {
@@ -9,12 +23,9 @@ public class ItlogNiDexter {
         int egg = 0, egg1 = 0, egg2 = 0, egg3 = 0;
         String[] balut = {"Penoy", "Balut#16", "Balut18"};
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Your Name: ");
-        name = in.next();
-        Scanner input = new Scanner(System.in);
-        for (String balut1 : balut) {
-            System.out.print("How many " + balut1 + ": ");
-            egg1 = egg =input.nextInt();
-        }
+//        Inventory customer = new Inventory;
+        Inventory customer = new Inventory("Dexter", 12, 15, 26);
+        System.out.println(Inventory.customer);
+        
     }
 }
