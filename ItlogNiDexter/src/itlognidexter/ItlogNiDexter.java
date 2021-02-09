2f1d1e71e79325151332a1f83509e596a524871e
iampatik <janpatrick.tancinco@student.passerellesnumeriques.org>
2021-02-09 15:39:36 +0800

    Updated

--- ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
+++ ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
@@ -2,30 +2,44 @@ package itlognidexter;
 
 import java.util.Scanner;
 
-class Inventory {
+class Order {
 
-    static boolean customer;
-    String name;
-    int penoy, balut16, balut18;
+    String customerName;
+    double amount;
+    double total;
+    EggVariant[] variants;
 
-    public Inventory(String name, int penoy, int balut16, int balut18) {
-        this.name = name;
-        this.penoy = penoy;
-        this.balut16 = balut16;
-        this.balut18 = balut18;
+    Order() {
+        this.variants = new EggVariant[5];
     }
+
+}
+
+class EggVariant {
+
+    String variant;
+    int qty;
+    double price;
 }
 
 public class ItlogNiDexter {
 
     public static void main(String[] args) {
-        String name = " ";
-        int egg = 0, egg1 = 0, egg2 = 0, egg3 = 0;
-        String[] balut = {"Penoy", "Balut#16", "Balut18"};
-        Scanner in = new Scanner(System.in);
-//        Inventory customer = new Inventory;
-        Inventory customer = new Inventory("Dexter", 12, 15, 26);
-        System.out.println(Inventory.customer);
-        
+
+        Scanner input = new Scanner(System.in);
+
+        String[] variants = {"Penoy", "Balot#16", "Balot#18"};
+
+        Order orders[] = new Order[10];
+
+        Order order = new Order();
+
+        order.customerName = "Loerm";
+
+        orders[0] = order;
+
+        System.out.println("Order 1 : Customer Name : " + orders[0].customerName);
+
     }
+
 }
