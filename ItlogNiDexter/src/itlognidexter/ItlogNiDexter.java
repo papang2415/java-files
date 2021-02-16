ad60b464a91e4021c6950d913178d214e7d567e2
iampatik <janpatrick.tancinco@student.passerellesnumeriques.org>
2021-02-16 13:11:45 +0800

    update

--- ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
+++ ItlogNiDexter/src/itlognidexter/ItlogNiDexter.java
@@ -2,10 +2,6 @@ package itlognidexter;
 
 import java.util.Scanner;
 
-
-
-
-
 public class ItlogNiDexter {
 
     public static void main(String[] args) {
@@ -13,41 +9,35 @@ public class ItlogNiDexter {
         Scanner input = new Scanner(System.in);
 
         String inventory = "";
-        int total =0;
-        
+        int total = 0;
+
         String order;
-        
-        
+
         System.out.println("Enter customer name");
         String name = input.nextLine();
         System.out.println("Enter customer variant");
         String variant = input.nextLine();
         System.out.println("Enter customer quantity");
         int qty = input.nextInt();
-        
+
         int subTotal = ItlogNiDexter.getSubTotal(variant, qty);
-        
-        order = "Customer name : " + name + "\n" +
-                "Variant : " + variant + "\n" +
-                 "Qty : " + qty + "\n" +
-                "SubTotal : " + subTotal + "\n";
-        
-        
-        
-        
+
+        order = "Customer name : " + name + "\n"
+                + "Variant : " + variant + "\n"
+                + "Qty : " + qty + "\n"
+                + "SubTotal : " + subTotal + "\n";
+
         System.out.println(order);
+
         inventory += order;
-        
+
         System.out.println("Inventory \n" + inventory);
-        
-        
-        
 
     }
-    
-    public static int getSubTotal(String variant,int qty){
+
+    public static int getSubTotal(String variant, int qty) {
         int subTotal = 0;
-        switch(variant){
+        switch (variant) {
             case "penoy":
                 subTotal = qty * 12;
                 break;
@@ -56,13 +46,11 @@ public class ItlogNiDexter {
                 break;
             case "balot#118":
                 subTotal = qty * 16;
-                break;    
-                
+                break;
+
         }
- 
-        return  subTotal;
+
+        return subTotal;
     }
-    
-    
 
 }
