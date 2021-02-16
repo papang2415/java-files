d11334f10c1adf48d846d984b71cc394cb5c8130
Papang2415 <tampiocdexter2415@gmail.com>
2021-02-16 18:42:49 +0800

    update

--- bitayBata/src/bitaybata/BitayBata.java
+++ bitayBata/src/bitaybata/BitayBata.java
@@ -6,44 +6,38 @@ public class BitayBata {
 
     public static void main(String[] args) {
         char temp;
-        /*String[] bata = {"(_)","_|_"," |","/ \\"};
-
-        String bata1 = "(_)";
-        String bata2 ="_|_";
-        String bata3 =" |";
-        String bata4 ="/ \\";
-        System.out.println(bata1);
-        System.out.println(bata2);
-        System.out.println(bata3);
-        System.out.println(bata4);*/
+        String[] bata = {"(_)", "_|_", " |", "/ \\"};
 
         test a = new test();
         int guess = a.generateRandom();
         String name = a.names[guess];
-        int len = a.names[guess].length();
+        int len = bata.length;
 
-        for (int i = 0; i < name.length(); i++) {
+        /* for (int i = 0; i < name.length(); i++) {
             System.out.print(" _ ");
-            System.out.print(" ");
-        }
-        System.out.println("\n" + a.names[guess]);
+        }*/
+        //System.out.println("\n" + a.names[guess]);
         System.out.println("Hint: " + a.hint[guess]);
 
         Scanner in = new Scanner(System.in);
 
+        String hide = "";
+
         do {
-            System.out.print("Enter Letter: ");
+            System.out.print("\nEnter Letter: ");
             temp = in.next().charAt(0);;
             if (temp == a.names[guess].charAt(0)) {
+                for (int i = 0; i < name.length(); i++) {
+                    hide += " _ ";
+                }
+                System.out.println(hide);
                 System.out.println("correct");
                 len = len;
             } else {
                 System.out.println("Wrong");
                 len--;
             }
-
         } while (len > 0);
-
     }
 
 }
