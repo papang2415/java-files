12dff99fd063751110d0516e13fd1286f8092c3a
Papang2415 <tampiocdexter2415@gmail.com>
2021-02-16 19:31:22 +0800

    new update

--- bitayBata/src/bitaybata/BitayBata.java
+++ bitayBata/src/bitaybata/BitayBata.java
@@ -12,32 +12,33 @@ public class BitayBata {
         int guess = a.generateRandom();
         String name = a.names[guess];
         int len = bata.length;
+        Scanner in = new Scanner(System.in);
+        String hide = "";
+        for (int i = 0; i < name.length(); i++) {
+            hide += " - ";
+        }
 
         /* for (int i = 0; i < name.length(); i++) {
             System.out.print(" _ ");
         }*/
-        //System.out.println("\n" + a.names[guess]);
+        System.out.println("\n" + a.names[guess]);
         System.out.println("Hint: " + a.hint[guess]);
-
-        Scanner in = new Scanner(System.in);
-
-        String hide = "";
 
         do {
             System.out.print("\nEnter Letter: ");
             temp = in.next().charAt(0);;
             if (temp == a.names[guess].charAt(0)) {
-                for (int i = 0; i < name.length(); i++) {
-                    hide += " _ ";
-                }
                 System.out.println(hide);
                 System.out.println("correct");
                 len = len;
             } else {
+                System.out.println(hide);
                 System.out.println("Wrong");
                 len--;
             }
+
         } while (len > 0);
+
     }
 
 }
